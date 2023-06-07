bca17b89e606ac68c083f88a4441a20def2499b1
민경 <tjalsrud0101@gmail.com>
2023-06-08 00:35:02 +0900

    initial commit

---
+++ src/main/java/se3/treecycle/TreecycleApplication.java
@@ -0,0 +1,13 @@
+package se3.treecycle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreecycleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreecycleApplication.class, args);
+	}
+
+}
